36d4c02f80c9c84ab2573a4aaff879b2c9312174
ghoshsubh <143290873+ghoshsubh@users.noreply.github.com>
2023-12-20 17:33:18 -0800

    Update the name of the directory again

---
+++ Llama2/SFT_with_FullBaseModel/data_NoRobots/FormData.py
@@ -0,0 +1,25 @@
+
+import pandas as pd
+import sys
+import numpy as np
+
+
+tokenizer_path = './tokenizer.model'
+
+sys.path.append('./FineTuneLLM/')
+from tokenizer import Tokenizer
+
+tokenizer = Tokenizer(model_path=tokenizer_path)
+
+
+def form_data_no_robots(file:str):
+  data = pd.read_parquet(file)
+  data = data['messages'].tolist()
+  
+  data_all =  '\n'.join(x['role'] + ':\n' + x['content'] for x1 in data for x in x1)
+  
+  x = np.array(tokenizer.encode(data_all, bos = False, eos = True))
+  #print(x)
+  return x
+
+
